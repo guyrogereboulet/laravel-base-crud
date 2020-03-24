336d751f17bd98302f2e5b005ea55f5e2d12a889
Guy Roger Eboulet <eboulet87@gmail.com>
2020-03-24 16:27:15 +0100

    Restyling Create

--- resources/views/shoes/create.blade.php
+++ resources/views/shoes/create.blade.php
@@ -1,26 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <form action=" {{route('shoes.store')}}" method='post'>
-        @csrf
-        <input type="text" name="ean" value="" placeholder="ean">
-        <input type="text" name="brand" value="" placeholder="brand">
-        <input type="text" name="price" value="" placeholder="price">
-        <input type="text" name="typology" value="" placeholder="typology">
-        <input type="text" name="genre" value="" placeholder="genre">
-        <input type="text" name="year" value="" placeholder="year">
-        @method('POST')
+@extends('layouts.layout')
 
-        <button type="submit" name="button">
-            Save
-        </button>
-    </form>
-    
-</body>
-</html>
+@section('header')
+  <h1>Insert a shoe</h1>
+@endsection
+
+@section('main')
+  <form action=" {{route('shoes.store')}}" method='post'>
+      {{-- è importante che questi token siano dentro il form --}}
+      @csrf
+      @method('POST')
+      <div class="">
+        <input type="text" name="ean" value="" placeholder="ean">
+      </div>
+      <div class="">
+        <input type="text" name="brand" value="" placeholder="brand">
+      </div>
+      <div class="">
+        <input type="text" name="price" value="" placeholder="price">
+      </div>
+      <div class="">
+        <input type="text" name="typology" value="" placeholder="typology">
+      </div>
+      <div class="">
+        <input type="text" name="genre" value="" placeholder="genre">
+      </div>
+      <div class="">
+        <input type="date" name="year" value="" placeholder="year">
+      </div>
+
+      <button type="submit" name="button">
+          Save
+      </button>
+  </form>
+
+
+@endsection
+
+@section('footer')
+@endsection
